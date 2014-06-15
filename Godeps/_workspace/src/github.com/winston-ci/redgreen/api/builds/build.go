93cfcd04b233f19cac10bdb23b43f7724f848732
Alex Suraci <suraci.alex@gmail.com>
2014-06-15 15:01:24 -0700

    update env format

--- Godeps/_workspace/src/github.com/winston-ci/redgreen/api/builds/build.go
+++ Godeps/_workspace/src/github.com/winston-ci/redgreen/api/builds/build.go
@@ -3,13 +3,13 @@ package builds
 import "time"
 
 type Build struct {
-	Guid      string      `json:"guid,omitempty"`
-	CreatedAt time.Time   `json:"created_at,omitempty"`
-	Image     string      `json:"image"`
-	Path      string      `json:"path,omitempty"`
-	Script    string      `json:"script"`
-	Env       [][2]string `json:"env"`
-	Status    string      `json:"status,omitempty"`
+	Guid      string              `json:"guid,omitempty"`
+	CreatedAt time.Time           `json:"created_at,omitempty"`
+	Image     string              `json:"image"`
+	Path      string              `json:"path,omitempty"`
+	Script    string              `json:"script"`
+	Env       []map[string]string `json:"env"`
+	Status    string              `json:"status,omitempty"`
 }
 
 type BuildResult struct {
