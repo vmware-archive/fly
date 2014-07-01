90b720879e3ca35bc7357950b1e598af6f04a7d3
Alex Suraci <suraci.alex@gmail.com>
2014-07-01 17:03:49 -0400

    deps

--- Godeps/_workspace/src/github.com/concourse/glider/api/builds/build.go
+++ Godeps/_workspace/src/github.com/concourse/glider/api/builds/build.go
@@ -1,15 +1,17 @@
 package builds
 
-import "time"
+import (
+	"time"
+
+	"github.com/concourse/turbine/api/builds"
+)
 
 type Build struct {
-	Guid      string              `json:"guid,omitempty"`
-	CreatedAt time.Time           `json:"created_at,omitempty"`
-	Image     string              `json:"image"`
-	Path      string              `json:"path,omitempty"`
-	Script    string              `json:"script"`
-	Env       []map[string]string `json:"env"`
-	Status    string              `json:"status,omitempty"`
+	Guid      string        `json:"guid,omitempty"`
+	CreatedAt time.Time     `json:"created_at,omitempty"`
+	Config    builds.Config `json:"config"`
+	Path      string        `json:"path,omitempty"`
+	Status    string        `json:"status,omitempty"`
 }
 
 type BuildResult struct {
---
+++ Godeps/_workspace/src/github.com/concourse/turbine/api/builds/build.go
@@ -0,0 +1,88 @@
+package builds
+
+type Status string
+
+const (
+	StatusStarted   Status = "started"
+	StatusSucceeded Status = "succeeded"
+	StatusFailed    Status = "failed"
+	StatusErrored   Status = "errored"
+)
+
+type Build struct {
+	Guid string `json:"guid"`
+
+	AbortURL string `json:"abort_url"`
+
+	Privileged bool `json:"privileged"`
+
+	Config Config `json:"config"`
+
+	LogsURL  string `json:"logs_url"`
+	Callback string `json:"callback"`
+
+	Inputs  []Input  `json:"inputs"`
+	Outputs []Output `json:"outputs"`
+
+	Status Status `json:"status"`
+}
+
+type Config struct {
+	Image  string            `json:"image" yaml:"image"`
+	Params map[string]string `json:"params" yaml:"params"`
+	Run    RunConfig         `json:"run" yaml:"run"`
+}
+
+type RunConfig struct {
+	Path string   `json:"path" yaml:"path"`
+	Args []string `json:"args" yaml:"args"`
+}
+
+type Input struct {
+	Name string `json:"name"`
+
+	Type string `json:"type"`
+
+	// e.g. sha
+	Version Version `json:"version,omitempty"`
+
+	// e.g. git url, branch, private_key
+	Source Source `json:"source"`
+
+	// e.g. commit_author, commit_date
+	Metadata []MetadataField `json:"metadata,omitempty"`
+
+	ConfigPath      string `json:"config_path"`
+	DestinationPath string `json:"destination_path"`
+}
+
+type Version map[string]interface{}
+
+type MetadataField struct {
+	Name  string `json:"name"`
+	Value string `json:"value"`
+}
+
+type Output struct {
+	Name string `json:"name"`
+
+	Type string `json:"type"`
+
+	// e.g. sha
+	Version Version `json:"version"`
+
+	// e.g. git url, branch, private_key
+	Source Source `json:"source"`
+
+	// arbitrary config for output
+	Params Params `json:"params,omitempty"`
+
+	// e.g. commit_author, commit_date, commit_sha
+	Metadata []MetadataField `json:"metadata,omitempty"`
+
+	SourcePath string `json:"sourcePath"`
+}
+
+type Source map[string]interface{}
+
+type Params map[string]interface{}
