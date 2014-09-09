a86c424d9e1df69185af1cb9578a3d68b517bfce
Alex Suraci <suraci.alex@gmail.com>
2014-09-08 20:04:24 -0700

    update for new atc api packages

--- Godeps/_workspace/src/github.com/concourse/atc/api/routes/routes.go
+++ Godeps/_workspace/src/github.com/concourse/atc/api/routes/routes.go
@@ -1,4 +1,4 @@
-package api
+package routes
 
 import "github.com/tedsuo/rata"
 
@@ -8,6 +8,10 @@ const (
 	BuildEvents = "BuildEvents"
 	AbortBuild  = "AbortBuild"
 	HijackBuild = "HijackBuild"
+
+	ListJobBuilds      = "ListJobBuilds"
+	GetJobBuild        = "GetJobBuild"
+	GetJobCurrentBuild = "GetJobCurrentBuild"
 
 	CreatePipe = "CreatePipe"
 	WritePipe  = "WritePipe"
@@ -22,6 +26,10 @@ var Routes = rata.Routes{
 	{Path: "/api/v1/builds/:build_id/events", Method: "GET", Name: BuildEvents},
 	{Path: "/api/v1/builds/:build_id/abort", Method: "POST", Name: AbortBuild},
 	{Path: "/api/v1/builds/:build_id/hijack", Method: "POST", Name: HijackBuild},
+
+	{Path: "/api/v1/jobs/:job_name/builds", Method: "GET", Name: ListJobBuilds},
+	{Path: "/api/v1/jobs/:job_name/builds/:build_name", Method: "GET", Name: GetJobBuild},
+	{Path: "/api/v1/jobs/:job_name/current-build", Method: "GET", Name: GetJobCurrentBuild},
 
 	{Path: "/api/v1/pipes", Method: "POST", Name: CreatePipe},
 	{Path: "/api/v1/pipes/:pipe_id", Method: "PUT", Name: WritePipe},
